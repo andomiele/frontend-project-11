47f13a3892904e7d80d0ed5446544590927fd324
Andomiele <alina2005@ya.ru>
2024-05-16 15:08:51 +0300

    adding conditions for displaying notifications

--- src/view.js
+++ src/view.js
@@ -1,12 +1,25 @@
 import onChange from 'on-change';
-import render from './render.js';
 
-const view = (state) => {
-  const watchedState = onChange(state, (path, newValue) => {
-    if (path === 'textError') {
-      render(state, newValue);
+const watch = (initialState, elements) => {
+  const state = onChange(initialState, (path, newValue) => {
+    if (path === 'fields.textError') {
+      const message = elements.feedback;
+      message.textContent = newValue;
+    }
+    if (path === 'fields.isValid') {
+      if (!initialState.fields.isValid) {
+        elements.feedback.classList.add('text-danger');
+        elements.input.classList.add('is-invalid');
+      }
+      if (initialState.fields.isValid) {
+        elements.form.reset();
+        elements.input.focus();
+        elements.feedback.classList.remove('text-danger');
+        elements.feedback.classList.add('text-success');
+        elements.input.classList.remove('is-invalid');
+      }
     }
   });
-  return watchedState;
+  return state;
 };
-export default view;
+export default watch;
