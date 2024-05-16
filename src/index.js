4697c50c29966f3ce662e23f83c9d2c3b8c78921
Andomiele <alina2005@ya.ru>
2024-05-16 15:12:05 +0300

    file content modification

--- src/index.js
+++ src/index.js
@@ -1,55 +1,6 @@
+// @ts-chec
 import './styles.scss';
 import 'bootstrap';
-import keyBy from 'lodash/keyBy.js';
-import * as yup from 'yup';
-import isEmpty from 'lodash/isEmpty.js';
-import view from './view.js';
+import app from './app.js';
 
-const schema = yup.object().shape({
-  website: yup.string().url().nullable().required('Ссылка должна быть валидным URL'),
-});
-
-const validate = (fields) => {
-  try {
-    schema.validateSync(fields, { abortEarly: false });
-    return {};
-  } catch (e) {
-    return keyBy(e.inner, 'path');
-  }
-};
-
-const app = async () => {
-  const form = document.querySelector('[class="form-control]');
-  const button = document.querySelector('[type="submit"]');
-  const state = {
-    fields: {
-      url: '',
-    },
-    urlList: [],
-    isValid: true,
-    textError: '',
-    postList: [],
-  };
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    state.fields.url = e.target.value;
-    const errors = validate(state.fields);
-    state.textError = errors;
-    state.isValid = isEmpty(errors);
-    const urlList = state.urlList.find((element) => element.name === state.fields.url);
-    if (urlList !== undefined) {
-      state.isValid = false;
-    }
-    state.urlList.push(e.target.value);
-    view(state);
-  });
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (state.isValid === false) {
-      view(state);
-    }
-  });
-  view(state);
-};
-export default app;
+app();
