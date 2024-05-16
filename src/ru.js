5ac0d252d8e0e6d880a35829e98c8acaf0621931
Andomiele <alina2005@ya.ru>
2024-05-16 15:07:09 +0300

    message library construction

---
+++ src/ru.js
@@ -0,0 +1,9 @@
+export default {
+  translation: {
+    invalidURL: 'Ссылка должна быть валидным URL',
+    invalidRSS: 'Ресурс не содержит валидный RSS',
+    emptyString: 'Строка не должна быть пустой',
+    validRSS: 'RSS успешно загружен',
+    existRSS: 'RSS уже существует',
+  },
+};
