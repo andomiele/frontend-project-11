0487035388fbbd698d4b80f79ec5debc9cee7be5
Andomiele <alina2005@ya.ru>
2024-05-21 00:10:57 +0300

    feat: add error output from i18next

--- src/view.js
+++ src/view.js
@@ -1,17 +1,20 @@
 import onChange from 'on-change';
 
-const watch = (initialState, elements) => {
+const watch = (initialState, elements, i18next) => {
   const state = onChange(initialState, (path, newValue) => {
-    if (path === 'fields.textError') {
-      const message = elements.feedback;
-      message.textContent = newValue;
+    const message = elements.feedback;
+    if (path === 'form.textError') {
+      message.textContent = i18next.t(`resources.translation.${newValue}`);
     }
-    if (path === 'fields.isValid') {
-      if (!initialState.fields.isValid) {
+    if (path === 'load.textError') {
+      message.textContent = i18next.t(`resources.translation.${newValue}`);
+    }
+    if (path === 'form.isValid') {
+      if (!initialState.form.isValid) {
         elements.feedback.classList.add('text-danger');
         elements.input.classList.add('is-invalid');
       }
-      if (initialState.fields.isValid) {
+      if (initialState.form.isValid) {
         elements.form.reset();
         elements.input.focus();
         elements.feedback.classList.remove('text-danger');
