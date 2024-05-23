c7ae1cdd2211785a930b545107e772ed55bd2b20
Andomiele <alina2005@ya.ru>
2024-05-23 14:10:11 +0300

    bug: Generate receipt of feeds and posts to pass to rendering

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,25 @@
-const parse = (response, initialState) => {
+const parse = (response) => {
   const parser = new DOMParser();
-  const any = parser.parseFromString(response, 'text/xml');
-  console.log(any);
-  if (any.querySelector('parsererror')) {
-    // eslint-disable-next-line no-param-reassign
-    initialState.parse.textError = 'loadError';
+  const doc = parser.parseFromString(response.contents, 'text/xml');
+  if (doc.querySelector('parsererror')) {
+    throw new Error('parseError');
   }
+  const feedTitle = doc.querySelector('channel > title').textContent;
+  const feedDescription = doc.querySelector('channel > description').textContent;
+  const postsCollection = doc.querySelectorAll('channel > item');
+  const postsList = Array.from(postsCollection);
+  const posts = postsList.map((element) => {
+    const title = element.querySelector('item > title').textContent;
+    const description = element.querySelector('item > description').textContent;
+    const link = element.querySelector('item > link').textContent;
+    return { title, description, link };
+  });
+  return ({
+    feed: {
+      title: feedTitle,
+      description: feedDescription,
+    },
+    postsList: posts,
+  });
 };
-
 export default parse;
