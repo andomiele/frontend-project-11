02a4ea05415f62a05265d359a837de7538df48d9
Andomiele <alina2005@ya.ru>
2024-05-25 16:31:09 +0300

    bug: Add text for empty string error

--- src/ru.js
+++ src/ru.js
@@ -2,6 +2,7 @@ export default {
   translation: {
     invalidURL: 'Ссылка должна быть валидным URL',
     invalidRSS: 'Ресурс не содержит валидный RSS',
+    emptyString: 'Не должно быть пустым',
     validRSS: 'RSS успешно загружен',
     existRSS: 'RSS уже существует',
     loadError: 'Ошибка сети',
